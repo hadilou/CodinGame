ebb4097212893c197d55c3a003b7f986ac3c53fd
Kayode hadilou ADJE <kayodehadilou@hotmail.com>
2020-04-13 00:13:46 +0300

    Wood2

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 
 /**
- * This code automatically collects game data in an infinite loop.
- * It uses the standard input to place data into the game variables such as x and y.
- * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
  **/
 
 int main()
@@ -16,21 +15,26 @@ int main()
 
     // game loop
     while (1) {
-        int x; // x position of your pod
-        int y; // y position of your pod
+        int x;
+        int y;
         int nextCheckpointX; // x position of the next check point
         int nextCheckpointY; // y position of the next check point
-        cin >> x >> y >> nextCheckpointX >> nextCheckpointY; cin.ignore();
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int opponentX;
+        int opponentY;
+        cin >> opponentX >> opponentY; cin.ignore();
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
-
-
-        // Edit this line to output the target position
-        // and thrust (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " " <<"100" << endl;
-    
-
+        
+        //Don't accelerate while backward
+        int thrust;
+        if (nextCheckpointAngle > 90 or nextCheckpointAngle <-90) {
+            thrust = 0;
+        }
+        else thrust = 100;
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
     }
 }
